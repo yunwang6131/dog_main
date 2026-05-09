7330d57ab932644236db6b35201bc87b1ba8c393
七昂 <2391701547@qq.com>
2026-05-09 09:22:38 +0800

    initial commit

---
+++ robot_lab/source/robot_lab/robot_lab/assets/dolanga.py
@@ -0,0 +1,82 @@
+
+"""Configuration for Dolanga1 quadruped robot."""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import DCMotorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+
+from robot_lab.assets import ISAACLAB_ASSETS_DATA_DIR
+
+
+DOLANGA1_CFG = ArticulationCfg(
+    spawn=sim_utils.UrdfFileCfg(
+        fix_base=False,
+        merge_fixed_joints=False,
+        replace_cylinders_with_capsules=False,
+        asset_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/dolanga1/urdf/dolanga1.urdf",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+        ),
+        joint_drive=sim_utils.UrdfConverterCfg.JointDriveCfg(
+            gains=sim_utils.UrdfConverterCfg.JointDriveCfg.PDGainsCfg(
+                stiffness=0,
+                damping=0,
+            )
+        ),
+    ),
+
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.50),
+        joint_pos={
+            ".*_hip_joint": 0.0,
+            ".*_thigh_joint": 0.8,
+            ".*_calf_joint": -1.5,
+        },
+        joint_vel={".*": 0.0},
+    ),
+
+    soft_joint_pos_limit_factor=0.9,
+
+    actuators={
+        "hip": DCMotorCfg(
+            joint_names_expr=[".*_hip_joint"],
+            effort_limit=96.0,
+            saturation_effort=96.0,
+            velocity_limit=23.0,
+            stiffness=40.0,
+            damping=1.0,
+            friction=0.0,
+        ),
+        "thigh": DCMotorCfg(
+            joint_names_expr=[".*_thigh_joint"],
+            effort_limit=156.0,
+            saturation_effort=156.0,
+            velocity_limit=23.0,
+            stiffness=40.0,
+            damping=1.0,
+            friction=0.0,
+        ),
+        "calf": DCMotorCfg(
+            joint_names_expr=[".*_calf_joint"],
+            effort_limit=156.0,
+            saturation_effort=156.0,
+            velocity_limit=14.0,
+            stiffness=40.0,
+            damping=1.0,
+            friction=0.0,
+        ),
+    },
+)
+"""Dolanga1 quadruped robot configuration using DC motors."""
---
+++ robot_lab/source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/quadruped/dolanga1/rough_env_cfg.py
@@ -0,0 +1,166 @@
+# Copyright (c) 2024-2026 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+from isaaclab.utils import configclass
+
+from robot_lab.tasks.manager_based.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
+
+##
+# Pre-defined configs
+##
+from robot_lab.assets.dolanga import DOLANGA1_CFG  # isort: skip
+
+
+@configclass
+class Dolanga1RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
+    base_link_name = "base_link"
+    # Allow both explicit foot links and merged-fixed-joint fallback on calf links.
+    foot_link_name = ".*_(foot|calf)_link"
+    # fmt: off
+    joint_names = [
+        "LF_hip_joint", "LF_thigh_joint", "LF_calf_joint",
+        "RF_hip_joint", "RF_thigh_joint", "RF_calf_joint",
+        "LH_hip_joint", "LH_thigh_joint", "LH_calf_joint",
+        "RH_hip_joint", "RH_thigh_joint", "RH_calf_joint",
+    ]
+    # fmt: on
+
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        # ------------------------------Sence------------------------------
+        self.scene.robot = DOLANGA1_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/" + self.base_link_name
+        self.scene.height_scanner_base.prim_path = "{ENV_REGEX_NS}/Robot/" + self.base_link_name
+        self.scene.terrain.max_init_terrain_level = 0
+
+        # ------------------------------Observations------------------------------
+        self.observations.policy.base_lin_vel.scale = 2.0
+        self.observations.policy.base_ang_vel.scale = 0.25
+        self.observations.policy.joint_pos.scale = 1.0
+        self.observations.policy.joint_vel.scale = 0.05
+        self.observations.policy.base_lin_vel = None
+        self.observations.policy.height_scan = None
+        self.observations.policy.joint_pos.params["asset_cfg"].joint_names = self.joint_names
+        self.observations.policy.joint_vel.params["asset_cfg"].joint_names = self.joint_names
+
+        # ------------------------------Actions------------------------------
+        # reduce action scale
+        self.actions.joint_pos.scale = {".*_hip_joint": 0.125, "^(?!.*_hip_joint).*": 0.25}
+        self.actions.joint_pos.clip = {".*": (-100.0, 100.0)}
+        self.actions.joint_pos.joint_names = self.joint_names
+
+        # ------------------------------Events------------------------------
+        self.events.randomize_reset_base.params = {
+            "pose_range": {
+                "x": (-0.5, 0.5),
+                "y": (-0.5, 0.5),
+                "z": (0.0, 0.2),
+                "roll": (0, 0),
+                "pitch": (0, 0),
+                "yaw": (-3.14, 3.14),
+            },
+            "velocity_range": {
+                "x": (0, 0),
+                "y": (0, 0),
+                "z": (0, 0),
+                "roll": (0, 0),
+                "pitch": (0, 0),
+                "yaw": (0, 0),
+            },
+        }
+        self.events.randomize_rigid_body_mass_base.params["asset_cfg"].body_names = [self.base_link_name]
+        self.events.randomize_rigid_body_mass_others.params["asset_cfg"].body_names = [
+            f"^(?!.*{self.base_link_name}).*"
+        ]
+        self.events.randomize_com_positions.params["asset_cfg"].body_names = [self.base_link_name]
+        self.events.randomize_apply_external_force_torque.params["asset_cfg"].body_names = [self.base_link_name]
+
+        # ------------------------------Rewards------------------------------
+        # General
+        self.rewards.is_terminated.weight = 0
+
+        # Root penalties
+        self.rewards.lin_vel_z_l2.weight = -2.0
+        self.rewards.ang_vel_xy_l2.weight = -0.05
+        self.rewards.flat_orientation_l2.weight = 0
+        self.rewards.base_height_l2.weight = 0
+        self.rewards.base_height_l2.params["target_height"] = 0.45
+        self.rewards.base_height_l2.params["asset_cfg"].body_names = [self.base_link_name]
+        self.rewards.body_lin_acc_l2.weight = 0
+        self.rewards.body_lin_acc_l2.params["asset_cfg"].body_names = [self.base_link_name]
+
+        # Joint penalties
+        self.rewards.joint_torques_l2.weight = -2.5e-5
+        self.rewards.joint_vel_l2.weight = 0
+        self.rewards.joint_acc_l2.weight = -2.5e-7
+        # self.rewards.create_joint_deviation_l1_rewterm("joint_deviation_hip_l1", -0.2, [".*_hip_joint"])
+        self.rewards.joint_pos_limits.weight = -5.0
+        self.rewards.joint_vel_limits.weight = 0
+        self.rewards.joint_power.weight = -2e-5
+        self.rewards.stand_still.weight = -2.0
+        self.rewards.joint_pos_penalty.weight = -1.0
+        self.rewards.joint_mirror.weight = -0.05
+        self.rewards.joint_mirror.params["mirror_joints"] = [
+            ["LF_(hip|thigh|calf).*", "RH_(hip|thigh|calf).*"],
+            ["RF_(hip|thigh|calf).*", "LH_(hip|thigh|calf).*"],
+        ]
+
+        # Action penalties
+        self.rewards.action_rate_l2.weight = -0.01
+
+        # Contact sensor
+        self.rewards.undesired_contacts.weight = -1.0
+        self.rewards.undesired_contacts.params["sensor_cfg"].body_names = [f"^(?!.*{self.foot_link_name}).*"]
+        self.rewards.contact_forces.weight = -1.5e-4
+        self.rewards.contact_forces.params["sensor_cfg"].body_names = [self.foot_link_name]
+
+        # Velocity-tracking rewards
+        self.rewards.track_lin_vel_xy_exp.weight = 3.0
+        self.rewards.track_ang_vel_z_exp.weight = 1.5
+
+        # Others
+        self.rewards.feet_air_time.weight = 0
+        self.rewards.feet_air_time.params["threshold"] = 0.5
+        self.rewards.feet_air_time.params["sensor_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_contact.weight = 0
+        self.rewards.feet_contact.params["sensor_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_contact_without_cmd.weight = 0.1
+        self.rewards.feet_contact_without_cmd.params["sensor_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_stumble.weight = 0
+        self.rewards.feet_stumble.params["sensor_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_slide.weight = 0
+        self.rewards.feet_slide.params["sensor_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_slide.params["asset_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_height.weight = 0
+        self.rewards.feet_height.params["target_height"] = 0.05
+        self.rewards.feet_height.params["asset_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_height_body.weight = 0
+        self.rewards.feet_height_body.params["target_height"] = -0.2
+        self.rewards.feet_height_body.params["asset_cfg"].body_names = [self.foot_link_name]
+        self.rewards.feet_gait.weight = 0
+        self.rewards.feet_gait.params["synced_feet_pair_names"] = (
+            ("LF_calf_link", "RH_calf_link"),
+            ("RF_calf_link", "LH_calf_link"),
+        )
+        self.rewards.upward.weight = 1.0
+
+        # If the weight of rewards is 0, set rewards to None
+        if self.__class__.__name__ == "Dolanga1RoughEnvCfg":
+            self.disable_zero_weight_rewards()
+
+        # ------------------------------Terminations------------------------------
+        # self.terminations.illegal_contact.params["sensor_cfg"].body_names = [self.base_link_name]
+        self.terminations.illegal_contact = None
+
+        # ------------------------------Curriculums------------------------------
+        # self.curriculum.command_levels_lin_vel.params["range_multiplier"] = (0.2, 1.0)
+        # self.curriculum.command_levels_ang_vel.params["range_multiplier"] = (0.2, 1.0)
+        self.curriculum.command_levels_lin_vel = None
+        self.curriculum.command_levels_ang_vel = None
+
+        # ------------------------------Commands------------------------------
+        # self.commands.base_velocity.ranges.lin_vel_x = (-1.5, 1.5)
+        # self.commands.base_velocity.ranges.lin_vel_y = (-0.8, 0.8)
+        # self.commands.base_velocity.ranges.ang_vel_z = (-1.5, 1.5)
