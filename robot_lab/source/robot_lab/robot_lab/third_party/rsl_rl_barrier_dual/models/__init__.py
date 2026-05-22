c931abf7208eb55354aac9008c461ac12f0c3d27
Yun Wang <yunwang6131@gmail.com>
2026-05-22 13:24:01 +0800

    edit_ignore

---
+++ robot_lab/source/robot_lab/robot_lab/third_party/rsl_rl_barrier_dual/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2024-2026 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+from .barrier_estimator_actor import BarrierEstimatorActor, BarrierEstimatorLosses
+
+__all__ = ["BarrierEstimatorActor", "BarrierEstimatorLosses"]
